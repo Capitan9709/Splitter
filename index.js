cda4a216ce317ccb8f2e4a212936462f9bafeea0
Capitan9709 <luismoyaprados@gmail.com>
2023-12-04 15:35:15 +0100

    primera subida, es funcional al 80%

---
+++ index.js
@@ -0,0 +1,80 @@
+var billElement = document.getElementById("bill");
+billElement.value = "";
+var billValue = 0;
+var peopleElement = document.getElementById("people");
+peopleElement.value = "";
+var peopleValue = 0;
+var tipResult = document.getElementById("tipResult");
+var totalResult = document.getElementById("totalResult");
+var tipButtons = document.getElementsByClassName("tipButton");
+var customTipElement = document.getElementById("customTip");
+customTipElement.value = "";
+customTipValue = 0;
+var resetButton = document.getElementById("resetButton");
+var tipValue = 0;
+
+// Funcion para que actualice valor de la cuenta
+billElement.addEventListener("keyup", function(){
+    billValue = billElement.value
+    // console.log(billValue)
+    updateTotal()
+});
+
+// Funcion para que actualice valor de las personas
+peopleElement.addEventListener("keyup", function(){
+    peopleValue = peopleElement.value
+    // console.log(peopleValue)
+    updateTotal()
+});
+
+// Funcion para que restaure las variables
+resetButton.addEventListener("click", function(){
+    billElement.value = ""
+    peopleElement.value = ""
+    totalResult.innerHTML = "0.00"
+    tipResult.innerHTML = "0.00"
+});
+
+// Funcion para obtener el valor de la propina
+for (let i = 0; i < tipButtons.length; i++){
+    tipButtons[i].addEventListener("click", function(){
+        // console.log(this.value)
+        for (let j = 0; j < tipButtons.length; j++){
+            tipButtons[j].classList.remove("activeButton")
+        }
+        this.classList.add("activeButton");
+        tipValue = this.value
+        updateTotal()
+    })
+}
+
+// Funcion para actualizar el valor personalizado de una propina
+customTipElement.addEventListener("keyup", function(){
+    customTipValue = customTipElement.value
+    tipButtons[5].value = customTipValue
+    // console.log(tipButtons[5].value)
+    tipValue = tipButtons[5].value
+    updateTotal()
+});
+
+// Funcion para que actualice valor del total
+function updateTotal(){
+    if(billValue == "" || peopleValue == ""){
+        totalResult.innerHTML = "0.00"
+        tipResult.innerHTML = "0.00"
+    }
+    else if(!tipValue){
+        totalResult.innerHTML = "0.00"
+        tipResult.innerHTML = "0.00"
+    }
+    else{
+        tip = (billValue * tipValue) / 100
+        tipResult.innerHTML = (tip / peopleValue).toFixed(2)
+        console.log(tipResult.innerHTML)
+
+        result = (billValue / peopleValue) + (tip / peopleValue)
+        totalResult.innerHTML = result.toFixed(2)
+        
+        console.log(totalResult.innerHTML)
+    }
+}
